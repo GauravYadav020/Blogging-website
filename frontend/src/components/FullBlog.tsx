8802162da0280a79f38a7afd575556b1f002642d
GauravYadav020 <itsgaurav6817@gmail.com>
2024-03-30 14:16:34 +0530

    dimensional changes

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -2,42 +2,41 @@ import { Blog } from "../hooks"
 import { Appbar } from "./Appbar"
 import { Avatar } from "./BlogCard"
 
-export const FullBlog = ({ blog }: {blog: Blog}) => {
-    return <div>
-        <Appbar />
-        <div className="flex justify-center">
-            <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
-                <div className="col-span-8">
-                    <div className="text-5xl font-extrabold">
+export const FullBlog = ({ blog }: { blog: Blog }) => {
+    return (
+        <div>
+            <Appbar />
+            <div className="px-4 sm:px-6 md:px-8 lg:px-10 w-full max-w-screen-xl pt-2">
+                <div className="text-slate-600 text-md mt-4 mb-2">
+                    {/* i remove the author */}
+                </div>
+                <div className="flex items-center">
+                    <div className="pr-4">
+                        <Avatar size="big" name={blog.author.name || "Anonymous"} />
+                    </div>
+                    <div>
+                        <div className="text-xl font-bold mb-1">
+                            {blog.author.name || "Anonymous"}
+                        </div>
+                        <div className="text-slate-500">
+                             Blogger and Content Creator
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="px-4 sm:px-6 md:px-8 lg:px-10 w-full max-w-screen-xl pt-8">
+                    <div className="text-3xl sm:text-5xl font-extrabold mb-4">
                         {blog.title}
                     </div>
-                    <div className="text-slate-500 pt-2">
+                    <div className="text-slate-500">
                         Post on 2nd December 2023
                     </div>
                     <div className="pt-4">
                         {blog.content}
                     </div>
                 </div>
-                <div className="col-span-4">
-                    <div className="text-slate-600 text-lg">
-                        Author
-                    </div>
-                    <div className="flex w-full">
-                        <div className="pr-4 flex flex-col justify-center">
-                            <Avatar size="big" name={blog.author.name || "Anonymous"} />
-                        </div>
-                        <div>
-                            <div className="text-xl font-bold">
-                                {blog.author.name || "Anonymous"}
-                            </div>
-                            <div className="pt-2 text-slate-500">
-                                "Welcome to the website! Let's dive into the world of blogging together. Share your thoughts, ideas, and stories with the world."
-                            </div>
-                        </div>
-                    </div>  
-                </div>
-                
             </div>
         </div>
-    </div>
+    );
 }
